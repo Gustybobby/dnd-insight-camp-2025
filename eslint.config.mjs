ecc6eac8490fca87f084d71e0cb6df17f28edd22
Gustybobby <napattatiyakaroonwong@gmail.com>
2025-03-21 21:42:11 +0700

    fix: fix lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    plugins: ["simple-import-sort"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: { project: "./tsconfig.json" },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            //type imports
+            ["\\u0000$"],
+            //anything else
+            ["^"],
+          ],
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
