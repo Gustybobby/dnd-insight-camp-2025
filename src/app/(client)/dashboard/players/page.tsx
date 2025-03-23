d8540a51b7eb2e55b04f5bb093c1bf91a8fdc72e
Gustybobby <napattatiyakaroonwong@gmail.com>
2025-03-23 16:09:10 +0700

    chore: remove test players query

--- src/app/(client)/dashboard/players/page.tsx
+++ src/app/(client)/dashboard/players/page.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import { getAllPlayers } from "@/server/controllers/player.controller";
-
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-
 import CreatePlayerForm from "@/components/dashboard/create-player/CreatePlayerForm";
 
 export default function PlayerDashboard() {
-  const { data: players, isRefetching: isPlayersRefetching } = useQuery({
-    queryKey: ["getAllPlayers"],
-    queryFn: async () => await getAllPlayers(),
-    refetchInterval: 5000,
-  });
-
   return (
     <div>
-      <div>
-        Test Players:{" "}
-        {isPlayersRefetching ? "refetching" : JSON.stringify(players)}
-      </div>
       <CreatePlayerForm />
     </div>
   );
