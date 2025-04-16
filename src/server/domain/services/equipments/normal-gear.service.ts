a85fe8191f04fbb2aa960bf49e9f84ea04b1e098
Gustybobby <napattatiyakaroonwong@gmail.com>
2025-04-17 03:34:40 +0700

    feat: add normal gear and weapon service

---
+++ src/server/domain/services/equipments/normal-gear.service.ts
@@ -0,0 +1,54 @@
+import type {
+  IEquipmentRepository,
+  IItemRepository,
+} from "@/server/domain/interfaces/repositories";
+import type {
+  IEffectService,
+  IEquipmentService,
+  IItemService,
+} from "@/server/domain/interfaces/services/applications";
+import type { PlayerEquipment } from "@/server/domain/models";
+
+import { BaseEquipmentService } from "@/server/domain/services/equipments/base.service";
+
+export class NormalGearService
+  extends BaseEquipmentService
+  implements IEquipmentService
+{
+  constructor(
+    equipmentRepo: IEquipmentRepository,
+    itemRepo: IItemRepository,
+    itemService: IItemService,
+    effectService: IEffectService,
+  ) {
+    super(
+      { part: "Gear" },
+      equipmentRepo,
+      itemRepo,
+      itemService,
+      effectService,
+    );
+  }
+
+  async onEquip({
+    playerId,
+    itemId,
+  }: Pick<PlayerEquipment, "playerId" | "itemId">): Promise<PlayerEquipment> {
+    const equipment = await super.onEquip({ playerId, itemId });
+
+    await this.modStats({ playerId, itemId });
+
+    return equipment;
+  }
+
+  async onRemove({
+    playerId,
+    itemId,
+  }: Pick<PlayerEquipment, "playerId" | "itemId">): Promise<void> {
+    await super.onRemove({ playerId, itemId });
+
+    await this.revertStats({ playerId, itemId });
+
+    return;
+  }
+}
